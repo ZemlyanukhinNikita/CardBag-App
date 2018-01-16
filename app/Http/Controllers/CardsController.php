ef903b27cdc4ed30abeff43d168c86f6659829a4
ZemlyanukhinNikita <nakeg8@gmail.com>
2018-01-16 19:41:37 +0700

    inject CardGenerateSevice

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -1,6 +1,8 @@
 <?php namespace App\Http\Controllers;
 
 use App\Service;
+use App\Service\CardGenerateService;
+use App\Service\CardService;
 
 class CardsController extends Controller
 {
@@ -8,11 +10,12 @@ class CardsController extends Controller
 
     /**
      * @param CardService $cardService
+     * @param CardGenerateService $cardGenerateService
      * @return \Illuminate\Http\JsonResponse
      */
-    public function getAllUserCards(CardService $cardService)
+    public function getAllUserCards(CardService $cardService, CardGenerateService $cardGenerateService)
     {
-        $cards = $cardService->checkUserCards();
+        $cards = $cardService->checkUserCards($cardGenerateService);
         if ($cards !== null) {
             return response()->json($cards, 200);
         } else {
