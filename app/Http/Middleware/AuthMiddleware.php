22337c6d35330a296e65fb614700eefdd057a1d6
roman0532 <kacher0532>
2018-01-18 20:26:41 +0700

    добавил код 404

--- app/Http/Middleware/AuthMiddleware.php
+++ app/Http/Middleware/AuthMiddleware.php
@@ -15,9 +15,15 @@ class AuthMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!empty($request->header('uuid'))) {
-            return $next($request);
+        if ($request->header('uuid')) {
+            if (!$uuid = preg_match('/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i',
+                $request->header('uuid'))
+            ) {
+                return response()->json(['status' => '400', 'message' => 'Invalid uuid'], 400);
+            }
+        } else {
+            return response()->json(['status' => '401', 'message' => 'Unauthorized'], 401);
         }
-        return response()->json(['status' => '401', 'message' => 'Unauthorized'], 401);
+        return $next($request);
     }
 }
