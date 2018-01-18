f9ec53f637417968e33fa5dacf5b548aea2df088
ZemlyanukhinNikita <nakeg8@gmail.com>
2018-01-18 20:29:47 +0700

    hid timestamps and field user_id

--- app/Card.php
+++ app/Card.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model
 {
+    public $timestamps = false;
+
+    protected $hidden = ['user_id'];
     
     public function categories()
     {
