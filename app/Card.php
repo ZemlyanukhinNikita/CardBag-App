fd1bcfaf2b905c6be5095218079796ed1d976074
ZemlyanukhinNikita <nakeg8@gmail.com>
2018-01-24 19:11:24 +0700

    add fillable fields

--- app/Card.php
+++ app/Card.php
@@ -8,6 +8,8 @@ class Card extends Model
         'discount' => 'int'
     ];
 
+    protected $fillable = ['user_id', 'title', 'category_id', 'front_photo', 'back_photo', 'discount'];
+
     public function category()
     {
         return $this->belongsTo(Category::class, 'category_id', 'id');
