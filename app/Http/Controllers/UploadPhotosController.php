72adaf63607aa254dd739451e4342a95ae2d9d24
roman0532 <kacher0532>
2018-01-29 19:22:11 +0700

    Redone authorization

--- app/Http/Controllers/UploadPhotosController.php
+++ app/Http/Controllers/UploadPhotosController.php
@@ -1,6 +1,5 @@
 <?php namespace App\Http\Controllers;
 
-use app\Repositories\UserInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -8,19 +7,14 @@ class UploadPhotosController extends Controller
 {
     /**
      * @param Request $request
-     * @param UserInterface $userRepository
      * @return \Illuminate\Http\JsonResponse
      * @throws \Symfony\Component\HttpFoundation\File\Exception\FileException
      */
-    public function uploadPhoto(Request $request, UserInterface $userRepository)
+    public function uploadPhoto(Request $request)
     {
         $this->validate($request, [
             'file' => 'required|image|max:20000',
         ]);
-
-        if (!$userRepository->findOneBy('uuid', $request->header('uuid'))) {
-            abort(401, 'Unauthorized');
-        }
 
         if ($request->file('file')->isValid()) {
             $filename = md5(time() . $request->file('file')) . '.' . $request->file('file')->guessExtension();
