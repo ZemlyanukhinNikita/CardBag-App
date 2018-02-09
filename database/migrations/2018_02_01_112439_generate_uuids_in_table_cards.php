4e0e04be584b79370788d621ced8600a2a357361
roman0532 <kacher0532>
2018-02-09 13:54:44 +0700

    deleted unused variables

--- database/migrations/2018_02_01_112439_generate_uuids_in_table_cards.php
+++ database/migrations/2018_02_01_112439_generate_uuids_in_table_cards.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Ramsey\Uuid\Uuid;
@@ -35,8 +34,5 @@ class GenerateUuidsInTableCards extends Migration
      */
     public function down()
     {
-        Schema::table('cards', function (Blueprint $table) {
-            //
-        });
     }
 }
