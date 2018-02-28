ad043d80eaa5ffbe22156d7d1ad8047f068fa15c
ZemlyanukhinNikita <nakeg8@gmail.com>
2018-02-28 12:59:38 +0700

    fix fabric

--- app/Service/SocialNetworkServiceFactory.php
+++ app/Service/SocialNetworkServiceFactory.php
@@ -3,37 +3,19 @@
 namespace App\Service;
 
 
-use app\Service\SocialNetworkCheckers\ConfigureFirebase;
-use GuzzleHttp\Client;
-
 class SocialNetworkServiceFactory
 {
-    private $client;
-    private $configureFirebase;
-
-    /**
-     * SocialNetworkServiceFactory constructor.
-     * @param Client $client
-     * @param ConfigureFirebase $configureFirebase
-     * @internal param $comfigureFirebase
-     */
-    public function __construct(Client $client, ConfigureFirebase $configureFirebase)
-    {
-        $this->client = $client;
-        $this->configureFirebase = $configureFirebase;
-    }
-
     public function getUserSocialToken($network)
     {
         switch ($network) {
             case 'Vk':
-                return new CheckUserTokenVkService($this->client);
+                return app(CheckUserTokenVkService::class);
             case 'Facebook':
-                return new CheckUserTokenFacebookService($this->client);
+                return app(CheckUserTokenFacebookService::class);
             case 'Google':
-                return new CheckUserTokenGoogleService($this->client);
+                return app(CheckUserTokenGoogleService::class);
             case 'Firebase':
-                return new CheckUserTokenFirebaseService($this->configureFirebase);
+                return app(CheckUserTokenFirebaseService::class);
         }
         abort(400, 'No implemented');
     }
